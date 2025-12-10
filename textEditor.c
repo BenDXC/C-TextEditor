e9dd6a336633df4872d6ade8320e4f3dd176d368
BenDXC <benlate0@gmail.com>
2025-12-10 16:59:32 +0000

    feat: cursor move functions

--- textEditor.c
+++ textEditor.c
@@ -9,9 +9,11 @@
 #include <errno.h>
 /** Defines */
 #define CTRL_KEY(k) ((k) & 0x1f)
+#define TEXTEDITOR_VERSION "0.0.1"
 /** Data */
 struct editorConfig
 {
+  int cx, cy;
   int screenrows;
   int screencols;
   struct termios orig_termios;
@@ -111,28 +113,72 @@ void abFree(struct abuf *ab)
 /** Output */
 void editorDrawRows(struct abuf *ab)
 {
-  for (int y = 0; y < E.screenrows; y++)
+  int y;
+  for (y = 0; y < E.screenrows; y++)
   {
-    abAppend(ab, "~", 1);
-    if (y < E.screenrows - 1)
-      abAppend(ab, "\r\n", 2);
+    if (y == E.screenrows / 3)
+    {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome),
+                                "C Text Editor -- version %s", TEXTEDITOR_VERSION);
+      if (welcomelen > E.screencols)
+        welcomelen = E.screencols;
+      int padding = (E.screencols - welcomelen) / 2;
+      if (padding)
+      {
+        abAppend(ab, "~", 1);
+        padding--;
+      }
+      while (padding--)
+        abAppend(ab, " ", 1);
+      abAppend(ab, welcome, welcomelen);
+    }
+    else
+      abAppend(ab, "~", 1);
   }
+  abAppend(ab, "\x1b[K", 3); /* Clear the line from cursor to the end */
+  if (y < E.screenrows - 1)
+    abAppend(ab, "\r\n", 2);
 }
 void editorRefreshScreen()
 {
   struct abuf ab = ABUF_INIT;
   abAppend(&ab, "\x1b[?25l", 6); /* Hide the cursor */
-  abAppend(&ab, "\x1b[2J", 4);   /* Clear the entire screen */
   abAppend(&ab, "\x1b[H", 3);    /* Move the cursor to the top-left corner */
 
   editorDrawRows(&ab);
-  abAppend(&ab, "\x1b[?25h", 6); /* Show the cursor */
-  abAppend(&ab, "\x1b[H", 3);    /* Move the cursor to the top-left corner */
 
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  abAppend(&ab, buf, strlen(buf)); /* Move the cursor to its current position */
+
+  abAppend(&ab, "\x1b[?25h", 6);      /* Show the cursor */
   write(STDOUT_FILENO, ab.b, ab.len); /* Write the buffer to the standard output */
   abFree(&ab);                        /* Free the allocated memory for the buffer */
 }
 /*** Input */
+void editorMoveCursor(char key)
+{
+  switch (key)
+  {
+  case 'h':
+    if (E.cx != 0)
+      E.cx--;
+    break;
+  case 'l':
+    if (E.cx != E.screencols - 1)
+      E.cx++;
+    break;
+  case 'k':
+    if (E.cy != 0)
+      E.cy--;
+    break;
+  case 'j':
+    if (E.cy != E.screenrows - 1)
+      E.cy++;
+    break;
+  }
+}
 void editorProcessKeypress()
 {
   char c = editorReadKey();
@@ -143,11 +189,19 @@ void editorProcessKeypress()
     write(STDOUT_FILENO, "\x1b[H", 3);  /* Move the cursor to the top-left corner */
     exit(0);
     break;
+    case 'w':
+    case 's':
+    case 'a':
+    case 'd':
+      editorMoveCursor(c == 'w' ? 'k' : c == 's' ? 'j' : c == 'a' ? 'h' : 'l'); /* Move cursor */
+      break;  
   }
 }
 /** Init System */
 void initEditor()
 {
+  E.cx = 0; // Initialize cursor x position
+  E.cy = 0; // Initialize cursor y position
   if (getWindowSize(&E.screenrows, &E.screencols) == -1)
     die("getWindowSize");
 }
